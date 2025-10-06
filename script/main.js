439d45230c5b9b4af8881120285800c43fcbad76
Ryuzuky <rizky.anugrahs@gmail.com>
2025-10-06 14:50:54 +0700

    Add certificate card content in skill section

--- script/main.js
+++ script/main.js
@@ -1,2 +1,30 @@
 var dupe = document.querySelector(".skill-slide").cloneNode(true);
 document.querySelector(".skill-list").appendChild(dupe);
+
+const certImages = document.querySelectorAll('.card-cert img');
+  const overlay    = document.getElementById('overlay');
+  const popupImg   = document.getElementById('popupImg');
+
+  certImages.forEach(img => {
+    img.addEventListener('click', () => {
+      popupImg.src = img.src; // langsung ambil src
+      overlay.style.display = 'flex';
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  // klik background overlay -> tutup popup
+  overlay.addEventListener('click', (e) => {
+    if (e.target === overlay) {
+      overlay.style.display = 'none';
+      document.body.style.overflow = 'auto';
+    }
+  });
+
+  // tombol ESC -> tutup popup
+  document.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+      overlay.style.display = 'none';
+      document.body.style.overflow = 'auto';
+    }
+  });
